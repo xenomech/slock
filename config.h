65adb643296918da54ba12b946664fe0e15ac140
xenomech <gokulsmenon227@gmail.com>
2021-06-06 19:55:52 +0530

    ⚡️ Media keys

--- config.h
+++ config.h
@@ -1,6 +1,6 @@
 /* user and group to drop privileges to */
-static const char *user  = "nobody";
-static const char *group = "nogroup";
+static const char *user  = "xenomech";
+static const char *group = "storage";
 
 static const char *colorname[NUMCOLS] = {
 	[INIT] = "black",       /* after initialization */
